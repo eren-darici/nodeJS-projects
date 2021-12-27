6578a00117942f91459b0fadaa62aa56b2df1d21
eren-darici <ed.erendarici@gmail.com>
2021-12-28 01:22:50 +0300

    Changes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,51 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 // Create Express Application
 const app = express();
 
-// Public files path
+// Config file paths
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsDirectoryPath = path.join(__dirname, '../templates/views');
+const partialsDirectoryPath = path.join(__dirname, '../templates/partials');
+
+
 
 // Set public folder as static
 app.use(express.static(publicDirectoryPath));
-// Set view engine
+
+// Set view engine and config handlebars
 app.set('view engine', 'hbs');
+app.set('views', viewsDirectoryPath);
+hbs.registerPartials(partialsDirectoryPath);
+
+
+// Home page
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Eren'
+    })
+})
+
+// About page
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Eren',
+        picture: 'img/pic.png'
+    })
+})
+
+// Weather page
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is a help message'
+    })
+})
+
 
 // ExpressJS
 app.get('/weather', (req, res) => {
@@ -18,6 +53,11 @@ app.get('/weather', (req, res) => {
         forecast: 'It is snowing',
         location: 'Philadelphia'
     });
+})
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.render('404', {title: '404', message: 'Page not found'})
 })
 
 // app.com
